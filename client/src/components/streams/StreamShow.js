bcd1427c410d7ab4433eb3666f737c0495a6c06c
Agarwal Neetu <agarwalneetu301@gmail.com>
2020-11-12 16:06:18 +0530

    Integrate RTMP Server

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import flv from 'flv.js';
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
 
 class StreamShow extends React.Component{
-    componentDidMount(){
-        this.props.fetchStream(this.props.match.params.id);
+    constructor(props){
+        super(props);
+        this.videoRef=React.createRef();
     }
-    render(){
+    componentDidMount(){
+        const id=this.props.match.params.id;
+        this.props.fetchStream(id);
+
+        this.player=flv.createPlayer({
+            type:'flv',
+            url:`http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+    }
+
+    componentWillUnmount(){
+        this.player.destroy();
+    }
+
+    renderStream(){
         if(!this.props.stream){
             return <div>Loading...</div>
+        }else{
+            return (
+                <div>
+                    <h1>{this.props.stream.title}</h1>
+                    <h5>{this.props.stream.description}</h5>
+                </div>
+            )
         }
+    }
+    render(){
         return (
             <div>
-                <h1>{this.props.stream.title}</h1>
-        <h5>{this.props.stream.description}</h5>
+                 <video ref={this.videoRef} style={{width:"100%"}} controls={true}/>
+                    {this.renderStream()}
             </div>
         )
     }
